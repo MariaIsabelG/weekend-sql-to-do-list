79893088e43e248848a2c24b15aef3c607a773fc
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-30 20:47:21 -0500

    set the part of the router:

---
+++ server/routes/tasks.router.js
@@ -0,0 +1,21 @@
+// declare variable for tools needed
+const {query} = require('express');
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export router
+module.exports = router;
